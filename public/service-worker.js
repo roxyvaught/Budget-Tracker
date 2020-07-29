8eab2bd7df3c2f0a5f10327b39fce46ac11c03f3
roxyvaught <roxanna.vaught@gmail.com>
2020-07-29 16:51:29 -0500

    change cached files

--- public/service-worker.js
+++ public/service-worker.js
@@ -3,19 +3,13 @@ const VERSION = 'version_01';
 const CACHE_NAME = APP_PREFIX + VERSION;
 
 const FILES_TO_CACHE = [
-  './index.html',
-  './css/styles.css',
-  './js/index.js',
-  './js/idb.js',
-  './manifest.json',
-  './icons/icon-512x512.png',
-  './icons/icon-384x384.png',
-  './icons/icon-192x192.png',
-  './icons/icon-152x152.png',
-  './icons/icon-144x144.png',
-  './icons/icon-128x128.png',
-  './icons/icon-96x96.png',
-  './icons/icon-72x72.png'
+'/',
+'./index.html',
+'./css/styles.css',
+'./js/index.js',
+'./js/idb.js',
+'./icons/icon-512x512.png',
+'./icons/icon-192x192.png',
 ];
 
 self.addEventListener('install', function(e) {
